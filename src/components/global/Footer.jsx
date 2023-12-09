0ea0b09896d86aae6a16a6e9598cd7f3c3b84b3a
YashSuvagiyaDeveloper <137169748+YashSuvagiyaDeveloper@users.noreply.github.com>
2023-12-09 10:54:37 +0530

    "Images"

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            Footer
+            <footer>
+                <div class="container">
+                    <div class="col-lg-8">
+                        <p>Copyright © 2048 Villa Agency Co., Ltd. All rights reserved.
+
+                            Design: <a rel="nofollow" href="https://templatemo.com" target="_blank">TemplateMo</a> Distribution: <a href="https://themewagon.com">ThemeWagon</a></p>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
